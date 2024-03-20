048236af8dd46defca5761ddaec767fc031dcd64
soCromp <snc40@pitt.edu>
2024-03-20 10:57:54 -0500

    simple buggy mlm sample

--- multihead_models.py
+++ multihead_models.py
@@ -33,11 +33,8 @@ class MultiheadLlamaForCausalLM(LlamaForCausalLM):
     def from_other(othermodel, orighead, masks, config):
         heads=[]
         
-        # in_features, out_features, bias = orighead.in_features, orighead.out_features, orighead.bias
-        # newhead = type(orighead)(in_features, out_features, bias)
-        # newhead.load_state_dict(orighead.state_dict())
-        # heads.append(newhead.to(orighead.weight.device))
-        heads.append(deepcopy(orighead))
+        for i in range(len(masks)):
+            heads.append(deepcopy(orighead))
         
         return MultiheadLlamaForCausalLM(othermodel, heads, config)
         
@@ -684,7 +681,170 @@ class MultiheadLlamaForCausalLM(LlamaForCausalLM):
         streamer: Optional["BaseStreamer"] = None,
         **model_kwargs,
     ) -> Union[GreedySearchOutput, torch.LongTensor]:
-        raise NotImplementedError('not implemented')
+        r"""
+            Parameters:
+                input_ids (`torch.LongTensor` of shape `(batch_size, sequence_length)`):
+                    The sequence used as a prompt for the generation.
+                logits_processor (`LogitsProcessorList`, *optional*):
+                    An instance of [`LogitsProcessorList`]. List of instances of class derived from [`LogitsProcessor`]
+                    used to modify the prediction scores of the language modeling head applied at each generation step.
+                stopping_criteria (`StoppingCriteriaList`, *optional*):
+                    An instance of [`StoppingCriteriaList`]. List of instances of class derived from [`StoppingCriteria`]
+                    used to tell if the generation loop should stop.
+
+                max_length (`int`, *optional*, defaults to 20):
+                    **DEPRECATED**. Use `logits_processor` or `stopping_criteria` directly to cap the number of generated
+                    tokens. The maximum length of the sequence to be generated.
+                pad_token_id (`int`, *optional*):
+                    The id of the *padding* token.
+                eos_token_id (`Union[int, List[int]]`, *optional*):
+                    The id of the *end-of-sequence* token. Optionally, use a list to set multiple *end-of-sequence* tokens.
+                output_attentions (`bool`, *optional*, defaults to `False`):
+                    Whether or not to return the attentions tensors of all attention layers. See `attentions` under
+                    returned tensors for more details.
+                output_hidden_states (`bool`, *optional*, defaults to `False`):
+                    Whether or not to return the hidden states of all layers. See `hidden_states` under returned tensors
+                    for more details.
+                output_scores (`bool`, *optional*, defaults to `False`):
+                    Whether or not to return the prediction scores. See `scores` under returned tensors for more details.
+                return_dict_in_generate (`bool`, *optional*, defaults to `False`):
+                    Whether or not to return a [`~utils.ModelOutput`] instead of a plain tuple.
+                synced_gpus (`bool`, *optional*, defaults to `False`):
+                    Whether to continue running the while loop until max_length (needed for ZeRO stage 3)
+                streamer (`BaseStreamer`, *optional*):
+                    Streamer object that will be used to stream the generated sequences. Generated tokens are passed
+                    through `streamer.put(token_ids)` and the streamer is responsible for any further processing.
+                model_kwargs:
+                    Additional model specific keyword arguments will be forwarded to the `forward` function of the model.
+                    If model is an encoder-decoder model the kwargs should include `encoder_outputs`.
+
+            Return:
+                [`~generation.GreedySearchDecoderOnlyOutput`], [`~generation.GreedySearchEncoderDecoderOutput`] or
+                `torch.LongTensor`: A `torch.LongTensor` containing the generated tokens (default behaviour) or a
+                [`~generation.GreedySearchDecoderOnlyOutput`] if `model.config.is_encoder_decoder=False` and
+                `return_dict_in_generate=True` or a [`~generation.GreedySearchEncoderDecoderOutput`] if
+                `model.config.is_encoder_decoder=True`.
+            ```"""
+        # from beam search. init values
+        logits_processor = logits_processor if logits_processor is not None else LogitsProcessorList()
+        stopping_criteria = stopping_criteria if stopping_criteria is not None else StoppingCriteriaList()
+        if max_length is not None:
+            warnings.warn(
+                "`max_length` is deprecated in this function, use"
+                " `stopping_criteria=StoppingCriteriaList([MaxLengthCriteria(max_length=max_length)])` instead.",
+                UserWarning,
+            )
+            stopping_criteria = validate_stopping_criteria(stopping_criteria, max_length)
+        pad_token_id = pad_token_id if pad_token_id is not None else self.generation_config.pad_token_id
+        eos_token_id = eos_token_id if eos_token_id is not None else self.generation_config.eos_token_id
+        if isinstance(eos_token_id, int):
+            eos_token_id = [eos_token_id]
+        eos_token_id_tensor = torch.tensor(eos_token_id).to(input_ids.device) if eos_token_id is not None else None
+        output_scores = output_scores if output_scores is not None else self.generation_config.output_scores
+        output_attentions = (
+            output_attentions if output_attentions is not None else self.generation_config.output_attentions
+        )
+        output_hidden_states = (
+            output_hidden_states if output_hidden_states is not None else self.generation_config.output_hidden_states
+        )
+        return_dict_in_generate = (
+            return_dict_in_generate
+            if return_dict_in_generate is not None
+            else self.generation_config.return_dict_in_generate
+        )
+
+        # init attention / hidden states / scores tuples
+        scores = () if (return_dict_in_generate and output_scores) else None
+        decoder_attentions = () if (return_dict_in_generate and output_attentions) else None
+        cross_attentions = () if (return_dict_in_generate and output_attentions) else None
+        decoder_hidden_states = () if (return_dict_in_generate and output_hidden_states) else None
+
+        # if model is an encoder-decoder, retrieve encoder attention weights and hidden states
+        if return_dict_in_generate and self.config.is_encoder_decoder:
+            encoder_attentions = model_kwargs["encoder_outputs"].get("attentions") if output_attentions else None
+            encoder_hidden_states = (
+                model_kwargs["encoder_outputs"].get("hidden_states") if output_hidden_states else None
+            )
+
+        # keep track of which sequences are already finished
+        unfinished_sequences = torch.ones(input_ids.shape[0], dtype=torch.long, device=input_ids.device)
+
+        this_peer_finished = False  # used by synced_gpus only
+        
+        # if synced_gpus:
+        #     # Under synced_gpus the `forward` call must continue until all gpus complete their sequence.
+        #     # The following logic allows an early break if all peers finished generating their sequence
+        #     this_peer_finished_flag = torch.tensor(0.0 if this_peer_finished else 1.0).to(input_ids.device)
+        #     # send 0.0 if we finished, 1.0 otherwise
+        #     dist.all_reduce(this_peer_finished_flag, op=dist.ReduceOp.SUM)
+        #     # did all peers finish? the reduced sum will be 0.0 then
+        #     if this_peer_finished_flag.item() == 0.0:
+        #         break
+
+        # prepare model inputs
+        model_inputs = self.prepare_inputs_for_generation(input_ids, **model_kwargs)
+
+        print('model inputs', model_inputs['input_ids'].shape)
+        
+        # from forward():
+        output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
+        output_hidden_states = (
+            output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
+        )
+        # return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        # decoder outputs consists of (dec_features, layer_state, dec_hidden, dec_attn)
+        outputs = self.model(
+            **model_inputs,
+            # input_ids=input_ids,
+            # attention_mask=attention_mask,
+            # position_ids=position_ids,
+            # past_key_values=past_key_values,
+            # inputs_embeds=inputs_embeds,
+            # use_cache=use_cache,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=True,
+        )
+        
+        if return_dict_in_generate:
+            if output_attentions:
+                decoder_attentions += (
+                    (outputs.decoder_attentions,) if self.config.is_encoder_decoder else (outputs.attentions,)
+                )
+                if self.config.is_encoder_decoder:
+                    cross_attentions += (outputs.cross_attentions,)
+
+            if output_hidden_states:
+                decoder_hidden_states += (
+                    (outputs.decoder_hidden_states,)
+                    if self.config.is_encoder_decoder
+                    else (outputs.hidden_states,)
+                )
+
+        hidden_states = outputs[0] # batch_size x tokens x 4096
+        print('hidden states', hidden_states.shape)
+        
+        locs_batch, locs_tok = torch.where(input_ids==3695) #special mask token ~
+        result = input_ids
+        # do lm head
+        for i in range(input_ids.shape[0]):
+            for h,j in enumerate(locs_tok):
+                print(i, j.item(), input_ids[i,j.item()])
+                logits = self.heads[h](hidden_states[:, :j.item(), :])
+                
+                # pre-process distribution
+                next_tokens_scores = logits_processor(input_ids[:, :j.item()], logits)
+                
+                # Store scores, attentions and hidden_states when required
+                if return_dict_in_generate and output_scores:
+                    scores += (next_tokens_scores,)
+                
+                # argmax
+                next_tokens = torch.argmax(next_tokens_scores, dim=-1) # batch_size x j
+                result[:, j.item()] = next_tokens[:, -1]
+
+        return result
     
     
     def greedy_search(
